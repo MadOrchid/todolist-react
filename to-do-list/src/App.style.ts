d738f334271f3d5e92891ff61fbdfef8ce074fa4
Leonardo Begnossi <89605267+MadOrchid@users.noreply.github.com>
2022-03-06 02:54:44 -0300

    'tentando-começar'

---
+++ to-do-list/src/App.style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: #0000;
+    color: #0000;
+    min-heigth: 100vh;
+    `;
+export const Area = styled.div`
+    margin: auto;
+    max-width: 980px;
+    padding: 10px
+`
